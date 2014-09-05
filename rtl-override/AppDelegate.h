51393a6a5be3e780338f3551902e60a52662c101
Sven-S. Porst <ssp-web@earthlingsoft.net>
2014-09-05 11:18:05 +0200

    Initial Commit

---
+++ rtl-override/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  rtl-override
+//
+//  Created by  Sven on 05.09.14.
+//  Copyright (c) 2014 earthlingsoft. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
